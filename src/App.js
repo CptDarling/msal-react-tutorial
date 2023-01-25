e9ac60b26a9c6bc121eba7e19db897387f4e29a2
Richard Holyoak <rholyoak@gmail.com>
2023-01-25 14:47:34 +0000

    Finishing up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { PageLayout } from './components/PageLayout';
 import { AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
 import { ProfileContent } from './components/ProfileContent';
+import reportWebVitals from './reportWebVitals';
 
 function App() {
   return (
@@ -17,3 +18,8 @@ function App() {
 }
 
 export default App;
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
--- src/components/ProfileData.jsx
+++ src/components/ProfileData.jsx
@@ -12,7 +12,8 @@ export const ProfileData = (props) => {
         <li><strong>First Name: </strong> {props.graphData.givenName}</li>
         <li><strong>Last Name: </strong> {props.graphData.surname}</li>
         <li><strong>Job Title:</strong> {props.graphData.jobTitle}</li>
-        <li><strong>Email: </strong> {props.graphData.userPrincipalName}</li>
+        <li><strong>Office Location:</strong> {props.graphData.officeLocation}</li>
+        <li><strong>Email: </strong> {props.graphData.userPrincipalName.toLowerCase()}</li>
         <li><strong>Business Phone:</strong> {props.graphData.businessPhones ? props.graphData.businessPhones[0] : ""}</li>
         <li><strong>Preferred Language:</strong> {props.graphData.preferredLanguage}</li>
         <li><strong>Id: </strong> {props.graphData.id}</li>
